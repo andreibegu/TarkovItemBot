8958616660800034fda0abbddd4c298c4a3ee727
Andrei <andrewmotion2@gmail.com>
2022-08-17 13:02:15 +0200

    Change playing status

--- Services/PresenceService.cs
+++ Services/PresenceService.cs
@@ -5,22 +5,25 @@ using Microsoft.Extensions.Options;
 using System.Threading;
 using System.Threading.Tasks;
 using TarkovItemBot.Options;
+using TarkovItemBot.Services.TarkovDatabase;
 
 namespace TarkovItemBot.Services
 {
     public class PresenceService : DiscordBotService
     {
-        private readonly BotOptions _config;
+        private readonly TarkovDatabaseClient _tarkov;
 
-        public PresenceService(IOptions<BotOptions> config)
+        public PresenceService(TarkovDatabaseClient tarkov)
         {
-            _config = config.Value;
+            _tarkov = tarkov;
         }
 
         protected override async ValueTask OnReady(ReadyEventArgs args)
         {
+            var total = await _tarkov.GetItemIndexAsync();
+
             await Bot.SetPresenceAsync(
-                new LocalActivity(_config.Prefix + "help", ActivityType.Playing));
+                new LocalActivity($"{total.Total} Tarkov items", ActivityType.Watching));
         }
     }
 }
